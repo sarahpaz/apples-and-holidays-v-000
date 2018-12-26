456829368ead0c69e67aa4658ae9f85dbc77a4fb
Sarah Paz <sarah.paz@gmail.com>
2018-12-26 01:36:59 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,8 +25,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter][:christmas] << supply
-  holiday_hash[:winter][:new_years] << supply
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 
@@ -59,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  " + holiday.to_s.split("_").collect { |word|  word.capitalize}.join(" ") + ": " + supplies.join(", ")
+      puts "  #{holiday.to_s.split("_").collect { |word|  word.capitalize }.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
